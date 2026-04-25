3f06dca50c135febc846a081f388ac05f80f6066
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-04-26 03:09:14 +0600

    add session and show navber user data

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -1,12 +1,27 @@
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import manlogo from '@/assets/user.png'
 import Navlink from './Navlink';
+import { useSession } from '@/lib/auth-client';
+import { signOut } from 'better-auth/api';
 
 
 
 const Navbar = () => {
+
+    const { data, ispanding } = useSession()
+    
+
+    if (ispanding) {
+        return <div>
+            loging...
+        </div>
+    }
+
+    const user = data?.user
+
     return (
         <div className='flex container mx-auto justify-between items-center my-5'>
             <div></div>
@@ -16,8 +31,13 @@ const Navbar = () => {
                 <li><Navlink href='/career'>Career</Navlink></li>
             </ul>
             <div className='flex items-center gap-2'>
-                <Image src={manlogo} width={40} height={40} alt='man'></Image>
-                <Link href={'/login'}><button className='btn bg-linear-to-r from-black to-gray-500 text-white'>Login</button></Link>
+                {user ? <div>
+                    <Image className=' rounded-full' src={user.image || manlogo} width={40} height={10} alt='man'></Image>
+                    <p className='text-black'>{user.name}</p>
+                    <button onClick={()=>signOut()} className='btn'>SingOut</button>
+                </div>
+                    : <Link href={'/login'}><button className='btn bg-linear-to-r from-black to-gray-500 text-white'>Login</button></Link>}
+
             </div>
         </div>
     );
